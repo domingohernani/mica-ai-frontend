0aff80d71a0c29f2b4abe71be4f74eac8ac57363
domingohernani <dev.hernani.domingo@gmail.com>
2025-12-14 15:17:06 +0800

    feat: add axios instance

---
+++ src/utils/axios.ts
@@ -0,0 +1,20 @@
+import { useStore } from "@/stores/useStore";
+import axios from "axios";
+import type { AxiosInstance } from "axios";
+
+export const api: AxiosInstance = axios.create({
+  baseURL: import.meta.env["VITE_API_BASE_URL"],
+  timeout: 30000,
+});
+
+// Use the token stored using the zustand
+api.interceptors.request.use(
+  async (config) => {
+    const token = useStore.getState().token;
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
