212f44a49d529593e67aae9e3aca0d4e55671c6b
domingohernani <dev.hernani.domingo@gmail.com>
2025-12-14 15:16:49 +0800

    refactor: wrap routes inside of App

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,26 +1,37 @@
+import App from "@/App";
 import CallbackPage from "@/features/auth/pages/callback-page";
 import LoginPage from "@/features/auth/pages/login-page";
 import SignupPage from "@/features/auth/pages/signup-page";
+import OnboardPage from "@/features/orgnanization/pages/onboard-page";
 import MainLayout from "@/layout/main-layout";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: "signup",
-    element: <SignupPage />,
-  },
-  {
-    path: "login",
-    element: <LoginPage />,
-  },
-  {
-    path: "callback",
-    element: <CallbackPage />,
-  },
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [],
+    element: <App />,
+    children: [
+      {
+        path: "signup",
+        element: <SignupPage />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "onboard",
+        element: <OnboardPage />,
+      },
+      {
+        path: "callback",
+        element: <CallbackPage />,
+      },
+      {
+        path: "/",
+        element: <MainLayout />,
+        children: [],
+      },
+    ],
   },
 ]);
 
