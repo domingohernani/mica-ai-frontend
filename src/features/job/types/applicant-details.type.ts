400d7685853726b6924235a69c1dcf6fd2784a6b
domingohernani <dev.hernani.domingo@gmail.com>
2026-05-20 15:21:58 +0800

    feat: enhance job application process by updating applicant details handling and removing success screen

--- src/features/job/types/applicant-details.type.ts
+++ src/features/job/types/applicant-details.type.ts
@@ -1,15 +1,15 @@
 export type ApplicantDetails = {
   firstName: string;
   lastName: string;
-  middleName: string;
+  middleName: string | null;
   dateOfBirth: string;
   email: string;
   phoneNumber: string;
   currentAddress: string;
-  currentJobTitle: string;
-  currentCompany: string;
+  currentJobTitle: string | null;
+  currentCompany: string | null;
   yearsOfExperience: number;
   expectedSalary: number;
   availability: "immediate" | "15_days" | "1_month" | "more_than_1_month";
-  professionalLinks: string;
+  professionalLinks: string | null;
 };
