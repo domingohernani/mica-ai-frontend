3a8b16a8f848e18d81b6666b1fb894ed510e4ccc
Hernani Domingo <dev.hernani.domingo@gmail.com>
2026-06-03 14:59:15 +0800

    wip: scheduling page refactoring

---
+++ src/features/hiring/constants/interview-schedule-duration.ts
@@ -0,0 +1,7 @@
+const INTERVIEW_DURATIONS = [
+  { value: "30", label: "30 min" },
+  { value: "45", label: "45 min" },
+  { value: "60", label: "1 hr" },
+  { value: "90", label: "1.5 hr" },
+];
+export default INTERVIEW_DURATIONS;
